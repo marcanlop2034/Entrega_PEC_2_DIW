5809a206d7f90924b41ff2284a19066856030cf5
mcanadas29 <marcanlop2034@gmail.com>
2025-03-28 18:00:33 +0100

    Revisión General y subida a PRE 28/03/2025

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
 'use strict';
+const pathPdf = './PEC 2.pdf'
+
+function muestraPdf(){
+    //alert('muestrapdf');
+    let elePadre = document.getElementById('footer');
+    let eleFrame = document.createElement('iframe');
+    eleFrame.setAttribute('src',pathPdf);
+    eleFrame.style.width='100%';
+    eleFrame.style.height='960px';
+    elePadre.appendChild(eleFrame);
+    let btnShowPdf = document.getElementById('btnFooter');
+    btnShowPdf.innerHTML='Cerrar PDF';
+}
+
+function ocultaPdf(){
+    let elePadre = document.getElementById('footer');
+    elePadre.setAttribute('visible','true');
+    let btnShowPdf = document.getElementById('btnFooter');
+    btnShowPdf.innerHTML='Ver PDF';
+}
 
 function inicio(){
-    //alert('Comenzamos con DIW 18/03/2025');
+    let btnShowPdf = document.getElementById('btnFooter');
+    if(btnShowPdf.innerHTML='Cerrar PDF'){
+        btnShowPdf.addEventListener('click', ocultaPdf, false);
+    }else{
+        btnShowPdf.addEventListener('click', muestraPdf, false);
+    }
+   
 }
 
 document.addEventListener('DOMContentLoaded', inicio, false);
